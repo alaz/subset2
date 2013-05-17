66a2ac1a51e9f5cb997d093acde1af5b53b7af7c
Alexander Azarov <azarov@osinka.ru>
2013-05-17 11:14:37 +0400

    another variant to write a recursive Field[T] [issue #2]

--- src/test/scala/Rec.scala
+++ src/test/scala/Rec.scala
@@ -12,9 +12,30 @@ object Rec {
       Some( DBO("id" -> rec.id, "children" -> rec.children)() )
   }
 
-  // reader
+  // reader, variant 1
+  /*
   import DocParser._
 
   implicit lazy val recField = Field({ case Doc(rec) => rec })
-  lazy val Doc: DocParser[Rec] = get[Int]("id") ~ get[List[Rec]]("children").opt map { case id ~ children => new Rec(id, children) }
+  lazy val Doc: DocParser[Rec] =
+    get[Int]("id") ~ get[List[Rec]]("children").opt map {
+      case id ~ children => new Rec(id, children)
+    }
+  */
+
+  // reader, variant 2
+  implicit object recField extends Field[Rec] {
+    import DocParser._
+
+    lazy val Doc: DocParser[Rec] =
+      get[Int]("id") ~ get[List[Rec]]("children").opt map {
+        case id ~ children => new Rec(id, children)
+      }
+
+    override def apply(o: Any): Option[Rec] =
+      PartialFunction.condOpt(o) {
+        case Doc(rec) => rec
+      }
+  }
+  def Doc = recField.Doc // so that we don't need to change the spec
 }
